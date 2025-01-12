64f071ffe97e3e39973d414ee620ef575aeeebda
nicoly05 <nicolyandrade@icloud.com>
2025-01-12 17:11:51 +0000

    navbar
    criação da navbar com os links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Logo from "../assets/React.png";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 function Navbar() {
   return (
     <div class="navbar">
       <div className="leftSide">
-        <img src={Logo} alt=""/>
+        <img src={Logo}/>
       </div>
       <div className="rightSide"></div>
+      <Link to="/">Home</Link>
+      <Link to="/Faturacao">Faturação</Link>
+      <Link to="/Requisitos">Requisitos</Link>
+      <Link to="/Simulacao">Simulação</Link>
     </div>
   )
 }
