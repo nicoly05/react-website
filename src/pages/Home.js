eb950814c97150adc2b448c691f21009d9a4276a
nicoly05 <nicolyandrade@icloud.com>
2025-01-12 18:52:45 +0000

    Home page nao muito bonita
    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
+import BannerImage from "../assets/HomePageImage.png";
+import "../styles/Home.css"
 
 function Home() {
   return (
-    <div>
-      Home teste
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+     <div className="headerContainer" 
+     >
+            <h1>Sistema de Faturação</h1>
+            <p></p>
+            <Link to="/Faturacao">
+            <button> Consulte sua Faturação</button>
+            </Link>
+            <Link to="/Requisitos">
+            <button> Requisitos necessários</button>
+            </Link>
+            <Link to="/Simulacao">
+            <button> Simule uma Fatura</button>
+            </Link>
+        </div>
     </div>
   )
 }
