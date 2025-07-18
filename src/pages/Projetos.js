187c7ad6109e5a43d5e812e022e211508547ead0
nicoly05 <nicolyandrade@icloud.com>
2025-07-18 16:36:22 +0100

    adicionei titulo e legenda em cima da imagem

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -15,18 +15,18 @@ import WhatsApp from '../assets/WhatsApp.png';
 
 
 const imagens = [
-  { id: 1, src: Instagram, categoria: 1, titulo: "Instagram", legenda: "Legenda" },
-  { id: 2, src: RTP, categoria: 3, titulo: "RTP", legenda: "Legenda" },
-  { id: 3, src: PingoDoce, categoria: 1, titulo: "PingoDoce", legenda: "Legenda" },
-  { id: 4, src: Lust, categoria: 7, titulo: "Lust", legenda: "Legenda" },
-  { id: 5, src: TikTok, categoria: 2, titulo: "TikTok", legenda: "Legenda" },
-  { id: 6, src: Continente, categoria: 2, titulo: "Continente", legenda: "Legenda" },
-  { id: 7, src: TwitterX, categoria: 5, titulo: "X", legenda: "Legenda" },
-  { id: 8, src: TGB, categoria: 2, titulo: "TGB", legenda: "Legenda" },
-  { id: 9, src: Google, categoria: 6, titulo: "Google", legenda: "Legenda" },
-  { id: 10, src: Apple, categoria: 3, titulo: "Apple", legenda: "Legenda" },
-  { id: 11, src: Wrestling, categoria: 1, titulo: "Wrestling", legenda: "Legenda" },
-  { id: 12, src: WhatsApp, categoria: 4, titulo: "WhatsApp", legenda: "Legenda" },
+  { id: 1, src: Instagram, categoria: 1, titulo: "Instagram", legenda: "Identidade Visual" },
+  { id: 2, src: RTP, categoria: 3, titulo: "RTP", legenda: "Captação de Fotos e Vídeos" },
+  { id: 3, src: PingoDoce, categoria: 1, titulo: "PingoDoce", legenda: "Identidade Visual" },
+  { id: 4, src: Lust, categoria: 7, titulo: "Lust", legenda: "Cobertura em tempo real" },
+  { id: 5, src: TikTok, categoria: 2, titulo: "TikTok", legenda: "Gestão de Redes" },
+  { id: 6, src: Continente, categoria: 2, titulo: "Continente", legenda: "Gestão de Redes" },
+  { id: 7, src: TwitterX, categoria: 5, titulo: "X", legenda: "Tráfego Pago" },
+  { id: 8, src: TGB, categoria: 2, titulo: "TGB", legenda: "Gestão de Redes" },
+  { id: 9, src: Google, categoria: 6, titulo: "Google", legenda: "Motion Design" },
+  { id: 10, src: Apple, categoria: 3, titulo: "Apple", legenda: "Captação de Fotos e Vídeos" },
+  { id: 11, src: Wrestling, categoria: 1, titulo: "Wrestling", legenda: "Identidade Visual" },
+  { id: 12, src: WhatsApp, categoria: 4, titulo: "WhatsApp", legenda: "Edição de Fotos e Vídeos" },
 ];
 
 function Projetos() {
@@ -53,10 +53,15 @@ function Projetos() {
       <div className="gradeImagens">
         {imagensFiltradas.map((img) => (
           <div className="imagemBox" key={img.id}>
-            <img src={img.src} alt={img.titulo} />
-            <h3>{img.titulo}</h3>
-            <p>{img.legenda}</p>
+            <div className="imagemConteudo">
+              <img src={img.src} alt={img.titulo} />
+              <div className="overlay">
+                <h3>{img.titulo}</h3>
+                <p>{img.legenda}</p>
+              </div>
+            </div>
           </div>
+
         ))}
       </div>
 
