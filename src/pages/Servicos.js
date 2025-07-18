83d0ce1abe7aa0923f379a594f33ae42a7087918
nicoly05 <nicolyandrade@icloud.com>
2025-07-18 13:09:08 +0100

    pagina de serviços ok

--- src/pages/Servicos.js
+++ src/pages/Servicos.js
@@ -34,20 +34,22 @@ const Servicos = () => {
         <li>Manual da marca</li>
         <li>Arte para eventos (convites, painéis, brindes)</li>
         <li>Personalização com propósito</li>
-      
       </ul>
+      <button className='buttonBoxRosa'>Exemplos</button>
     </div>
   </div>
 
   {/* Box 3 - Preto, imagem à direita */}
   <div className="boxPreto">
     <div className="boxColuna boxTexto">
-      <h2>Outro Serviço</h2>
+      <h2>02. Gestão de Redes Sociais</h2>
       <ul>
-        <li>Legenda A</li>
-        <li>Legenda B</li>
-        <li>Legenda C</li>
+        <li>Planejamento de conteúdo estratégico</li>
+        <li>Criação de posts criativos</li>
+        <li>Legendas que conectam</li>
+        <li>Agendamento e organizações</li>
       </ul>
+      <button className='buttonBoxPreto'>Exemplos</button>
     </div>
     <div className="boxColuna boxImagem">
       <img src={MetricasImg} alt="Serviço 1" />
@@ -59,27 +61,28 @@ const Servicos = () => {
       <img src={MetricasImg} alt="Serviço 1" />
     </div>
     <div className="boxColuna boxTexto">
-      <h2>01. Identidade Visual</h2>
+      <h2>03. Captação de fotos e vídeos</h2>
       <ul>
-        <li>Logotipo</li>
-        <li>Paleta de Cores e Tipografia</li>
-        <li>Manual da marca</li>
-        <li>Arte para eventos (convites, painéis, brindes)</li>
-        <li>Personalização com propósito</li>
-      
+        <li>Ensaios para marcas e produtos</li>
+        <li>Bastidores</li>
+        <li>Vídeos institucionais e promocionais</li>
+        <li>Ensaios em estúdio ou externos</li>
       </ul>
+      <button className='buttonBoxRosa'>Exemplos</button>
     </div>
   </div>
 
   {/* Box 3 - Preto, imagem à direita */}
   <div className="boxPreto">
     <div className="boxColuna boxTexto">
-      <h2>Outro Serviço</h2>
+      <h2>04. Edição de fotos e vídeos</h2>
       <ul>
-        <li>Legenda A</li>
-        <li>Legenda B</li>
-        <li>Legenda C</li>
+        <li> Tratamento de imagens</li>
+        <li>Montagem de reels e vídeos</li>
+        <li>Edição de eventos</li>
+        <li>Padrão visual da sua marca</li>
       </ul>
+      <button className='buttonBoxPreto'>Exemplos</button>
     </div>
     <div className="boxColuna boxImagem">
      <img src={MetricasImg} alt="Serviço 1" />
@@ -91,29 +94,49 @@ const Servicos = () => {
    <img src={MetricasImg} alt="Serviço 1" />
     </div>
     <div className="boxColuna boxTexto">
-      <h2>Título do Serviço</h2>
+      <h2>05. Tráfego Pago</h2>
       <ul>
-        <li>Legenda 1</li>
-        <li>Legenda 2</li>
-        <li>Legenda 3</li>
+        <li>Anúncios no Instagram e Facebook</li>
+        <li>Campanhas com objetivos claros</li>
+        <li>Segmentação do público Ideal</li>
+        <li>Relatórios e otimizações</li>
       </ul>
+      <button className='buttonBoxRosa'>Exemplos</button>
     </div>
   </div>
 
   {/* Box 3 - Preto, imagem à direita */}
   <div className="boxPreto">
     <div className="boxColuna boxTexto">
-      <h2>Outro Serviço</h2>
+      <h2>06. Motion Design</h2>
       <ul>
-        <li>Legenda A</li>
-        <li>Legenda B</li>
-        <li>Legenda C</li>
+       <li>Animação de logo simples, entre 3 a 10 segundos</li>
+       <li>Vídeo para publicidade em motion design</li>
+       <li>Animação entre 30 a 60 segundos</li>
       </ul>
+       <button className='buttonBoxPreto'>Exemplos</button>
     </div>
     <div className="boxColuna boxImagem">
    <img src={MetricasImg} alt="Serviço 1" />
     </div>
   </div>
+
+   {/* Box 2 - Rosa, imagem à esquerda */}
+  <div className="boxRosa">
+    <div className="boxColuna boxImagem">
+   <img src={MetricasImg} alt="Serviço 1" />
+    </div>
+    <div className="boxColuna boxTexto">
+      <h2>07. Cobertura em Tempo Real</h2>
+      <ul>
+       <li>Stories durante o evento</li>
+       <li>Reels e conteúdo dinâmico</li>
+       <li>Posts atualizados no feed</li>
+      </ul>
+      <button className='buttonBoxRosa'>Exemplos</button>
+    </div>
+  </div>
+
 </div>
 
   );
