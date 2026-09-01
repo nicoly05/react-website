886548fb19c2f467289a8fa42f81250558832688
nicoly05 <nicolyandrade@icloud.com>
2026-09-01 10:54:40 +0100

    correções nas categorias dos serviços e projetos de cada marca parceira

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -7,6 +7,7 @@ import BenficaCascaisMalveira from '../assets/BenficaCascaisMalveira.png';
 import Ruche from '../assets/Ruche.png';
 import WrestleFest from '../assets/WrestleFest.png';
 import UAPovoense from '../assets/UAPovoense.png';
+import TEMPORARIO from '../assets/temporario.jpeg';
 import { useSearchParams } from 'react-router-dom';
 
 
@@ -14,50 +15,234 @@ const imagens = [
   { 
     id: 1, 
     src: WrestleFest, 
-    categoria: [1, 10], 
+    categoria: [1, 2, 3, 4, 7, 8, 9, 10], 
     titulo: "WrestleFest", 
-    legenda: "Identidade Visual",
-    descricao: "Desenvolvimento de identidade visual para WrestleFest"
+    legenda: "Identidade Visual, Gestão de Redes, Captação e Edição de Fotos e Vídeos, Cobertura em tempo real, Produção Audiovisual, Design Gráfico e Eventos",
+    descricao: "Desenvolvimento de identidade visual, gestão de redes sociais, captação e edição de fotos e vídeos, cobertura em tempo real, produção audiovisual, design gráfico e eventos para WrestleFest"
   },
   { 
     id: 2, 
     src: JanahEstetica, 
-    categoria: [3,11], 
+    categoria: [3, 4, 11], 
     titulo: "Janah", 
-    legenda: "Captação de Fotos e Vídeos",
-    descricao: "Captação de fotos e vídeos para RTP"
+    legenda: "Captação e edição de Fotos e Vídeos",
+    descricao: "Captação e edição de fotos e vídeos para RTP"
   },
   { 
     id: 3, 
     src: BenficaCascaisMalveira, 
-    categoria: [1, 12], 
-    titulo: "Benfica Cascais Malveira", 
-    legenda: "Identidade Visual",
-    descricao: "Identidade visual e gestão de redes sociais para Benfica Cascais Malveira"
+    categoria: [1, 2, 3, 4, 12], 
+    titulo: " Escola Benfica Cascais Malveira", 
+    legenda: "Identidade Visual, Gestão de Redes, Captação e Edição de Fotos e Vídeos",
+    descricao: "Identidade visual, gestão de redes sociais e captação e edição de fotos e vídeos para Benfica Cascais Malveira"
   },
   { 
     id: 4, 
     src: UAPovoense, 
-    categoria: [7, 12], 
+    categoria: [3, 4, 12],
     titulo: "UAPovoense", 
-    legenda: "Cobertura em tempo real",
-    descricao: "Cobertura em tempo real para UAPovoense"
+    legenda: "Captação e edição de Fotos e Vídeos",
+    descricao: "Captação e edição de fotos e vídeos para UAPovoense"
   },
   { 
     id: 5, 
     src: Victory, 
-    categoria: [2, 12], 
+    categoria: [3, 4, 12], 
     titulo: "Victory", 
-    legenda: "Gestão de Redes",
-    descricao: "Gestão de redes sociais para Victory"
+    legenda: "Captação e edição de Fotos e Vídeos",
+    descricao: "Captação e edição de fotos e vídeos para Victory"
   },
   { 
     id: 6, 
     src: Ruche, 
-    categoria: [2, 13], 
+    categoria: [3, 13], 
     titulo: "Ruche", 
+    legenda: "Captação e edição de Fotos e Vídeos",
+    descricao: "Captação e edição de fotos e vídeos para Ruche"
+  },
+  { 
+    id: 7, 
+    src: TEMPORARIO, 
+    categoria: [2, 13], 
+    titulo: "Café Portugal", 
     legenda: "Gestão de Redes",
-    descricao: "Gestão de redes sociais para Ruche"
+    descricao: "Gestão de redes sociais para Café Portugal"
+  },
+  { 
+    id: 8, 
+    src: TEMPORARIO, 
+    categoria: [2, 13], 
+    titulo: "Café Gelo", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão de redes sociais para Café Gelo"
+  },
+  { 
+    id: 9, 
+    src: TEMPORARIO, 
+    categoria: [2, 13], 
+    titulo: "Taverna do Pescador", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão de redes sociais para Taverna do Pescador"
+  },
+  { 
+    id: 10, 
+    src: TEMPORARIO, 
+    categoria: [2, 13], 
+    titulo: "Villa di Roma", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão de redes sociais para Villa di Roma"
+  },
+  { 
+    id: 11, 
+    src: TEMPORARIO, 
+    categoria: [3, 13], 
+    titulo: "Gelo Augusta", 
+    legenda: "Captação de Fotos e Vídeos",
+    descricao: "Captação e edição de fotos e vídeos para Gelo Augusta"
+  },
+  { 
+    id: 12, 
+    src: TEMPORARIO, 
+    categoria: [2, 13], 
+    titulo: "Restaurante Ouro", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão de redes sociais para Restaurante Ouro"
+  },
+  { 
+    id: 13, 
+    src: TEMPORARIO, 
+    categoria: [2, 13], 
+    titulo: "Brasa Lisboa", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão de redes sociais para Brasa Lisboa"
+  },
+  { 
+    id: 14, 
+    src: TEMPORARIO, 
+    categoria: [2, 12], 
+    titulo: "Ginásio 100%", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão de redes sociais para Ginásio 100%"
+  },
+    { 
+    id: 15, 
+    src: TEMPORARIO, 
+    categoria: [2, 11], 
+    titulo: "Ana Ferreira Hair", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão de redes sociais para Ana Ferreira Hair"
+  },
+    { 
+    id: 16, 
+    src: TEMPORARIO, 
+    categoria: [3, 4], 
+    titulo: "Julita Imóveis", 
+    legenda: "Captura e edição de fotos e vídeos",
+    descricao: "Captura e edição de fotos e vídeos para Julita Imóveis"
+  },
+    { 
+    id: 17, 
+    src: TEMPORARIO, 
+    categoria: [2, 3, 4, 10], 
+    titulo: "Bailo", 
+    legenda: "Gestão de Redes, Captação e edição de fotos e vídeos",
+    descricao: "Gestão de redes sociais, captação e edição de fotos e vídeos para Bailo"
+  },
+    { 
+    id: 18, 
+    src: TEMPORARIO, 
+    categoria: [2, 12], 
+    titulo: "Tribx", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão do Tik Tok para Tribx"
+  },
+    { 
+    id: 19, 
+    src: TEMPORARIO, 
+    categoria: [2, 14], 
+    titulo: "Villa Dias Alojamento Local", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão de redes sociais para Villa Dias Alojamento Local"
+  },
+   { 
+    id: 20, 
+    src: TEMPORARIO, 
+    categoria: [2], 
+    titulo: "Edite Psicologa", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão de redes sociais para Edite Psicologa"
+  },
+   { 
+    id: 21, 
+    src: TEMPORARIO, 
+    categoria: [2, 3, 4, 12], 
+    titulo: "Top Player", 
+    legenda: "Gestão de Redes, Captação e edição de fotos e vídeos",
+    descricao: "Gestão de redes, captação e edição de fotos e vídeos para Top Player"
+  },
+   { 
+    id: 22, 
+    src: TEMPORARIO, 
+    categoria: [2], 
+    titulo: "Academia Barril", 
+    legenda: "Gestão de Redes",
+    descricao: "Gestão de redes sociais para Academia Barril"
+  },
+   { 
+    id: 23, 
+    src: TEMPORARIO, 
+    categoria: [3, 4], 
+    titulo: "Gabriela Rosa", 
+    legenda: "Captura e edição de fotos e vídeos",
+    descricao: "Captura e edição de fotos e vídeos para Gabriela Rosa"
+  },
+  { 
+    id: 24, 
+    src: TEMPORARIO, 
+    categoria: [3, 4, 12], 
+    titulo: "DS", 
+    legenda: "Captação e edição de fotos e vídeos",
+    descricao: "Captação e edição de fotos e vídeos para DS"
+  },
+  { 
+    id: 25, 
+    src: TEMPORARIO, 
+    categoria: [3, 11], 
+    titulo: "Lidiane", 
+    legenda: "Captação e edição de fotos e vídeos",
+    descricao: "Captação e edição de fotos e vídeos para Lidiane"
+  },
+  { 
+    id: 26, 
+    src: TEMPORARIO, 
+    categoria: [3, 7, 10], 
+    titulo: "Skechers - Nós Alive", 
+    legenda: "Captação de fotos e vídeos em tempo real",
+    descricao: "Captação de fotos e vídeos em tempo real para Skechers - Nós Alive"
+  },
+   { 
+    id: 27, 
+    src: TEMPORARIO, 
+    categoria: [3, 7, 10], 
+    titulo: "Novex - Rock in Rio", 
+    legenda: "Captação de fotos e vídeos em tempo real",
+    descricao: "Captação de fotos e vídeos em tempo real para Novex - Rock in Rio"
+  },
+   { 
+    id: 28, 
+    src: TEMPORARIO, 
+    categoria: [3, 7, 10], 
+    titulo: "Visit Azores - BTL", 
+    legenda: "Captação de fotos e vídeos em tempo real",
+    descricao: "Captação de fotos e vídeos em tempo real para Visit Azores - BTL"
+  },
+   { 
+    id: 29, 
+    src: TEMPORARIO, 
+    categoria: [3, 7, 10], 
+    titulo: "Congresso Advogados", 
+    legenda: "Captação de fotos e vídeos em tempo real",
+    descricao: "Captação de fotos e vídeos em tempo real para Congresso Advogados"
   },
 ];
 
@@ -89,6 +274,7 @@ function Projetos() {
         <button className={filtro === 11 ? 'active' : ''} onClick={() => setFiltro(11)}>Estética & Moda</button>
         <button className={filtro === 12 ? 'active' : ''} onClick={() => setFiltro(12)}>Desporto</button>
         <button className={filtro === 13 ? 'active' : ''} onClick={() => setFiltro(13)}>Gastronomia</button>
+        <button className={filtro === 14 ? 'active' : ''} onClick={() => setFiltro(14)}>Turismo</button>
       </div>
 
       <div className="gradeImagens">
